6934af70bdc72ccadaa3f5d5509b080ba3c74605
ali-ataeva <a.606777083@gmail.com>
2025-07-06 22:48:13 +0200

    vitkova navarka

--- script.js
+++ script.js
@@ -10,40 +10,58 @@ function multiply(first, second) {
 function divide(first, second) {
     return first / second;
 }
+
+function getNumber(promptForUser) {
+    while(true){
+        let conversionResult = Number(prompt(promptForUser).replace(",", "."));
+        if(!isNaN(conversionResult)){
+            return conversionResult
+        }
+        console.warn("spatny cislo debile")
+    }
+}
+
+const operations = {
+    "+": add,
+    "-": subtract,
+    "/": divide,
+    "*": multiply,
+}
+
 while (true) {
     let firstNum;
     let secondNum;
     let operator;
-    while (true) {
-        firstNum = Number(prompt("Zadejte první číslo: ").replace(",", "."));
-        secondNum = Number(prompt("Zadejte druhé číslo: ").replace(",", "."));
-        if (!(isNaN(firstNum))&&!(isNaN(secondNum))) {
-            break;
-        }
-        else { continue; }
-    }
-    while (true) {
+
+
+    firstNum = getNumber("Zadejte první číslo: ")
+    secondNum = getNumber("Zadejte druhé číslo: ")
+
+
+    while (true) { // domaci ukol extrahovat do funkce!!
         operator = prompt('vyberte operaci(+|-|/|*): ');
         if (operator == '+'||'-'||'/'||'*') {
             break;
         }
-        else { continue; }
+        // else { continue; }
     }
-    if (operator == "+") {
-        console.log(`Váš výsledek je: ${add(firstNum, secondNum)}`)
+    // switch (operator) {
+    //     case "+":
+    //         console.log(`Váš výsledek je: ${add(firstNum, secondNum)}`);
+    //         break;
+    //     case "-":
+    //         console.log(`Váš výsledek je: ${subtract(firstNum, secondNum)}`);
+    //         break;
+    //     case "*":
+    //         console.log(`Váš výsledek je: ${multiply(firstNum, secondNum)}`);
+    //         break;
+    //     default:
+    //         console.log(`Váš výsledek je: ${divide(firstNum, secondNum)}`);
+    // }
+    let wantedOperation = operations[operator]
+    console.log(`Váš výsledek je: ${wantedOperation(firstNum, secondNum)}`);
+    let continueDecision = prompt('Přejete si pokračovat?(ano/NE): ') // nechat usera vedet o defaultni moznosti
+    if (continueDecision.toLocaleLowerCase != 'ano') { // toleruje "Ano"
+        break;
     }
-    else if (operator == "-"){
-        console.log(`Váš výsledek je: ${subtract(firstNum, secondNum)}`)
-    }
-    else if (operator == "*"){
-        console.log(`Váš výsledek je: ${multiply(firstNum, secondNum)}`)
-    }
-    else{
-        console.log(`Váš výsledek je: ${divide(firstNum, secondNum)}`)
-    }
-    let continueDecision = prompt('Přejete si pokračovat?(ano/ne): ')
-    if (continueDecision == 'ano') {
-        continue;
-    }
-    else { break; }
 }
