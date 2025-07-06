704893c4a716703439e9301853e47ffcd4fa643a
ali-ataeva <a.606777083@gmail.com>
2025-07-06 19:13:06 +0200

    Added functions and operator check

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-let firstNum = prompt('Vložte první číslo: ')
-let secundNum = prompt('Vložte druhé číslo: ')
-let operator = prompt('vyberte operaci +|-|/|*: ')
+function add(first, second) {
+    return first + second;
+}
+function subtract(first, second) {
+    return first - second;
+}
+function multiply(first, second) {
+    return first * second;
+}
+function divide(first, second) {
+    return first / second;
+}
+while (true) {
+    let firstNum = prompt('Vložte první číslo: ');
+    let secundNum = prompt('Vložte druhé číslo: ');
+    while (true) {
+        let operator = prompt('vyberte operaci +|-|/|*: ');
+        if (operator == "+"||"-"||"/"||"*") {
+            break;
+        }
+        else { continue; }
+    }
+    
+}
