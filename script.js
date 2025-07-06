98075d7518c9837a9398f3b4487e37a4bf7a2b11
ali-ataeva <a.606777083@gmail.com>
2025-07-06 20:41:45 +0200

    rookie mistake nedoslo mi ze ve for loopu nemuzu deklarovat promeny jinak kod bezi jupi)

--- script.js
+++ script.js
@@ -11,16 +11,19 @@ function divide(first, second) {
     return first / second;
 }
 while (true) {
+    let firstNum;
+    let secondNum;
+    let operator;
     while (true) {
-        const firstNum = Number(prompt("Zadejte první číslo: ").replace(",", "."));
-        const secondNum = Number(prompt("Zadejte druhé číslo: ").replace(",", "."));
+        firstNum = Number(prompt("Zadejte první číslo: ").replace(",", "."));
+        secondNum = Number(prompt("Zadejte druhé číslo: ").replace(",", "."));
         if (!(isNaN(firstNum))&&!(isNaN(secondNum))) {
             break;
         }
         else { continue; }
     }
     while (true) {
-        let operator = prompt('vyberte operaci(+|-|/|*): ');
+        operator = prompt('vyberte operaci(+|-|/|*): ');
         if (operator == '+'||'-'||'/'||'*') {
             break;
         }
